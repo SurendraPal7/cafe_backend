89165d9af1f9c69fa02d1df38f874f0023742aa7
NewUserName <ramanpal7228@gmail.com>
2025-07-08 15:13:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import Router from "./routes/userRoute.js";
+import { authenticate, authorize } from "./middlewares/auth.js";
+
+const app = express();
+app.use(express.json());
+const dbuser=encodeURIComponent(process.env.DBUSER) 
+const dbpass=encodeURIComponent(process.env.DBPASS)
+
+
+// mongoose.connect(`mongodb://localhost:27017/mern-cafe`).then(() => {
+//   app.listen(8000, () => {
+//     console.log("Server started");
+//   });
+// });
+
+
+// const dbuser=encodeURIComponent(process.env.DBUSER) 
+// const dbpass=encodeURIComponent(process.env.DBPASS)
+
+
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.ucggsmz.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=Cluster0
+`).then(() => {
+  app.listen(8000, () => {
+    console.log("Server started");
+  });
+});
+
+app.use("/api/users", Router);
